0a102596ce8422670b1938f7b55d6a3f54a46ea3
Priyanka Dilip Shinde <shinde.priyanka.15ee1156@gmail.com>
2022-04-27 02:06:29 -0400

    updates

---
+++ Assignment 1/data-ingestion-and-visualization-using-gcp/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A repo for structuring big data project for production level deployment',
+    author='kshitij,priyanka,dwithika',
+    license='MIT',
+)
